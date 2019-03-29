99fb12632c740f74c363d4ec4aff9857603ca933
yauheni_chyrkouski <evgen.box@live.ru>
2019-03-29 17:53:57 +0300

    tests

---
+++ src/components/movie-item/movie-item.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MovieItem } from './movie-item';
+
+test('Footer should render correctly', () => {
+    const movieObj = {
+        id: 0,
+        title: 'Green Book 1',
+        genre: 'Comedy',
+        description: 'Descr text',
+        imageURL: 'https://st.kp.yandex.net/images/film_iphone/iphone360_1108577.jpg',
+        year: '2018',
+        rating: '8.3',
+        duration: '120'
+    }
+    const component = renderer.create(
+        <MovieItem movie={movieObj} />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+})
